bc76601e7ce19f5e96ceaa70c4a9799753fcd44c
Zahra Vasanji <zahravasanji@hotmail.com>
2024-08-26 13:15:54 +0100

    added project descriptions and updated images

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -8,17 +8,50 @@ export const Projects = () => {
         fullStack={true}
         imageSrc={"/assets/holidough.png"}
         title={"HoliDough"}
-        description={"Budgeting app"}
-        tools={["React", "Node", "Express", "Material UI", "GSAP"]}
+        description={"Full stack holiday budgeting app using various tools for data manipulation and a Node backend sourcing data from a MySQL database"}
+        tools={["React", "Node", "Express", "MySQL", "Material UI", "GSAP"]}
         githubLink="https://github.com/zedvas/travel-tally"
         githubLinkBackend="https://github.com/zedvas/travel-tally-back-end"
         demoLink="https://holidough.uk/"
       />
+            <Card
+        imageSrc={"/assets/rent-a-pet-mobile.png"}
+        title={"Shopping Cart"}
+        description={"Ecommerce app using JavaScript functionality to add to cart, dynamically calculate totals and like items"}
+        tools={["React", "Redux", "CSS"]}
+        githubLink="https://github.com/zedvas/shopping-cart"
+        demoLink="https://rent-a-pet.netlify.app/"
+      />
       <Card
-        imageSrc={"/assets/julyLiebe.png"}
+        imageSrc={"/assets/simpsons-mobile.png"}
+        title={"Simpsons Quotes"}
+        description={"Search, like and delete quotes from data accessed using Fetch API"}
+        tools={["React Hooks", "Axios", "JavaScript"]}
+        githubLink="https://github.com/zedvas/simpsons"
+        demoLink="https://simpsons-quotes-zv.netlify.app/"
+      />
+      <Card
+        imageSrc={"/assets/altaloma-mobile.png"}
+        title={"AltaLoma"}
+        description={"Front end"}
+        tools={["React", "Material UI", "SASS"]}
+        githubLink="https://github.com/zedvas/altaloma-frontend"
+        demoLink="https://altaloma.netlify.app/"
+      />
+      <Card
+        imageSrc={"/assets/weather-mobile.png"}
+        title={"Weather App"}
+        description={"Vanilla JS project using the fetch API"}
+        tools={["Fetch API", "DOM Manipulation", "SASS"]}
+        githubLink="https://github.com/zedvas/weatherApp"
+        demoLink="https://weather-zv.netlify.app/"
+      />
+
+      <Card
+        imageSrc={"/assets/julyliebe-mobile.png"}
         title={"JulyLiebe Homage"}
-        description={"Vanilla app?"}
-        tools={["React", "Node", "Express", "Material UI", "GSAP"]}
+        description={"Static recreation of JulyLiebe homepage"}
+        tools={["HTML", "CSS"]}
         githubLink="https://github.com/zedvas/julyliebe2"
         demoLink="https://julyliebe.netlify.app/"
       />
@@ -30,38 +63,6 @@ export const Projects = () => {
         githubLink=""
         githubLinkBackend=""
         demoLink=""
-      />
-      <Card
-        imageSrc={"/assets/shoppingCart.png"}
-        title={"Shopping Cart"}
-        description={"ecommerce app"}
-        tools={["React", "Node", "Express", "Material UI", "GSAP"]}
-        githubLink="https://github.com/zedvas/shopping-cart"
-        demoLink="https://rent-a-pet.netlify.app/"
-      />
-      <Card
-        imageSrc={"/assets/simpsons.png"}
-        title={"Simpsons Quotes"}
-        description={"API usage"}
-        tools={["React", "Node", "Express", "Material UI", "GSAP"]}
-        githubLink="https://github.com/zedvas/simpsons"
-        demoLink="https://simpsons-quotes-zv.netlify.app/"
-      />
-      <Card
-        imageSrc={"/assets/altaloma.png"}
-        title={"AltaLoma"}
-        description={"Front end"}
-        tools={["React", "Node", "Express", "Material UI", "GSAP"]}
-        githubLink="https://github.com/zedvas/altaloma-frontend"
-        demoLink="https://altaloma.netlify.app/"
-      />
-      <Card
-        imageSrc={"/assets/weatherapp.png"}
-        title={"Weather App"}
-        description={"Vanilla JS project using the fetch API"}
-        tools={["React", "Node", "Express", "Material UI", "GSAP"]}
-        githubLink="https://github.com/zedvas/weatherApp"
-        demoLink="https://weather-zv.netlify.app/"
       />
     </div>
   );
