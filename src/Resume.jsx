c2a52f301c3b54d42b77fdd5890050d45b96bfa4
Zahra Vasanji <zahravasanji@hotmail.com>
2024-09-10 00:18:16 +0100

    styling

---
+++ src/Resume.jsx
@@ -0,0 +1,47 @@
+import { NavLink } from "react-router-dom";
+
+export const Resume = () => {
+  return (
+    <div className="resume">
+        <div className="resumeLink">
+      <h2><NavLink to="/ZahraVasanji.pdf" download target="_blank">
+        Click here</NavLink> to download a copy of my resume
+      </h2></div>
+      <div className="resumeIcons">
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/eslint/eslint-original-wordmark.svg" /><figcaption>ES Lint</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg" /><figcaption>GitHub</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original-wordmark.svg" /><figcaption>Express</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg" /><figcaption>Vite JS</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/materialui/materialui-original.svg" /><figcaption>Material UI</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg" /><figcaption>Node JS</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg" /><figcaption>HTML5</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-plain-wordmark.svg" /><figcaption>MySQL</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original-wordmark.svg" /><figcaption>Git</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg" /><figcaption>React</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg" /><figcaption>CSS3</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg" /><figcaption>SASS</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original-wordmark.svg" /><figcaption>Bootstrap</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" /><figcaption>JavaScript</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg" /><figcaption>MongoDB</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/graphql/graphql-plain-wordmark.svg" /><figcaption>GraphQL</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original-wordmark.svg" /><figcaption>VSCode</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bash/bash-original.svg" /><figcaption>Bash</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/npm/npm-original-wordmark.svg" /><figcaption>NPM</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/threejs/threejs-original-wordmark.svg" /><figcaption>Three JS</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/socketio/socketio-original-wordmark.svg" /><figcaption>Socket io</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cypressio/cypressio-original-wordmark.svg" /><figcaption>Cypress io</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" /><figcaption>TypeScript</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/neo4j/neo4j-original-wordmark.svg" /><figcaption>Neo4j</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/firebase/firebase-original-wordmark.svg" /><figcaption>Firebase</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original-wordmark.svg" /><figcaption>NextJS</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original-wordmark.svg" /><figcaption>Docker</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/kubernetes/kubernetes-original-wordmark.svg" /><figcaption>Kubernetes</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/oauth/oauth-original.svg" /><figcaption>OAuth</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/netlify/netlify-original-wordmark.svg" /><figcaption>Netlify</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitest/vitest-original.svg" /><figcaption>ViTest</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/d3js/d3js-original.svg" /><figcaption>D3</figcaption></figure>
+        <figure><img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg" /><figcaption>AWS</figcaption></figure>
+      </div>
+    </div>
+  );
+};
